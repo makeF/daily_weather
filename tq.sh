35069b4ee97d8f73a214830ffb5b148a9ca6d643
makeF <2427829589@qq.com>
2021-01-31 00:55:17 +0800

    Update tq.sh

--- tq.sh
+++ tq.sh
@@ -3,7 +3,7 @@
 # 获取天气图片
 #curl zh.wttr.in/Chengdu_1MQn.png -o tq.png 
 #生成待发送md文档
-echo "\![tiqnq][tq] \
+echo "\![tiqnq][tq]  \
 [tq]:data:image/png;base64,`base64 -w 0 tq.png`" > t01
 #server酱推送到微信
 tq=`cat ./t01`
