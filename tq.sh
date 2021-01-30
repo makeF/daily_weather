5bc387e7f594a26cb516fd420c7b73b29223c853
makeF <2427829589@qq.com>
2021-01-31 00:53:43 +0800

    Update tq.sh

--- tq.sh
+++ tq.sh
@@ -7,5 +7,5 @@ echo "\![tiqnq][tq] \
 [tq]:data:image/png;base64,`base64 -w 0 tq.png`" > t01
 #server酱推送到微信
 tq=`cat ./t01`
-t=`date "+%Y-%m-%d "`
+t=`date "+%Y_%m_%d_"`
 curl --data-urlencode "desp=${tq}" https://sc.ftqq.com/SCU126376Td123af634152e7d8fe2ce3ac38c5a3595fae40493e52e.send?text=${t}今日天气
